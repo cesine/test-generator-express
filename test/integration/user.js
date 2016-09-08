e2746dc51c0c3c2d3e3700bfe4a158e652780867
cesine <cesine@yahoo.com>
2016-09-08 01:34:37 -0400

    reverting test back to basics too

--- test/integration/user.js
+++ test/integration/user.js
@@ -7,92 +7,20 @@ var supertest = require('supertest');
 var api = require('./../../');
 
 describe('/users', function() {
-    var user = {
-        givenName: 'Anony',
-        familyName: 'Mouse'
-    };
+  describe('GET', function() {
+    it('should list users', function(done) {
+      supertest(api)
+        .get('/users')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
 
-    describe('POST', function() {
-        it('should create', function(done) {
-            supertest(api)
-                .post('/users')
-                .send(user)
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end(function(err, res) {
-                    if (err) {
-                        return done(err);
-                    }
+          expect(res.text).to.equal('respond with a resource');
 
-                    debug('created ', res.body);
-                    expect(res.body).to.deep.equal({
-                        id: res.body.id,
-                        givenName: user.givenName,
-                        familyName: user.familyName
-                    });
-                    user.id = res.body.id;
-
-                    done();
-                });
+          done();
         });
     });
-
-    describe('GET', function() {
-        it('should list users', function(done) {
-            supertest(api)
-                .get('/users')
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end(function(err, res) {
-                    if (err) {
-                        return done(err);
-                    }
-
-                    expect(res.body).to.deep.equal({
-                        [user.id]: user
-                    });
-
-                    done();
-                });
-        });
-
-        it('should get a user', function(done) {
-            supertest(api)
-                .get('/users/' + user.id)
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end(function(err, res) {
-                    if (err) {
-                        return done(err);
-                    }
-
-                    expect(res.body).to.deep.equal(user);
-
-                    done();
-                });
-        });
-    });
-
-    describe('PUT', function() {
-        it('should update', function(done) {
-            user.description = 'A sample user';
-
-            supertest(api)
-                .put('/users/' + user.id)
-                .send(user)
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end(function(err, res) {
-                    if (err) {
-                        return done(err);
-                    }
-
-                    debug('updated ', res.body);
-                    expect(res.body).to.deep.equal({
-                        id: user.id,
-                        givenName: user.givenName,
-                        familyName: user.familyName,
-                        description: user.description
-                    });
-
-                    done();
-                });
-        });
-    });
+  });
 });
